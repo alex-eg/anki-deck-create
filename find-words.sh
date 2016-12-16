436b736fc8b03146bd250c599a483b97efac9d87
taptap <alexey.e.egorov@gmail.com>
2016-12-16 17:04:35 +0300

    add usage function to wrapper script

--- find-words.sh
+++ find-words.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 # Find all provided words
 
+usage() {
+        cat <<EOF
+Usage: find-words.sh [options] word...
+Options:
+  --fuzzy        Find inexact matches.
+
+EOF
+}
+
 words=""
 options=""
+
+if [[ $# -eq 0 ]]; then
+        usage
+        exit 1
+fi
 
 while [[ $# -gt 0 ]]; do
         case $1 in
                 --fuzzy)
                         fuzzy=1
                         options="$options -v inexact=1"
+                        ;;
+                --*)
+                        usage
+                        exit 1
                         ;;
                 *)
                         words="$1 $words"
