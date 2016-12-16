18d1f8b733ffe5aaf440a9ad5aa7af3c5b05ad4a
taptap <alexey.e.egorov@gmail.com>
2016-12-16 17:00:13 +0300

    add arguments and parsing to wrapper script
    Option --fuzzy enables inexcat matching in awk script.
    Also words are now collected into array.

--- find-words.sh
+++ find-words.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 # Find all provided words
 
-gawk -f parse.awk JMdict $*
+words=""
+options=""
+
+while [[ $# -gt 0 ]]; do
+        case $1 in
+                --fuzzy)
+                        fuzzy=1
+                        options="$options -v inexact=1"
+                        ;;
+                *)
+                        words="$1 $words"
+                        ;;
+        esac
+        shift
+done
+
+gawk -f parse.awk $options JMdict $words
+exit $?
